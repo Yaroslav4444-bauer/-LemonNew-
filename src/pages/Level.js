9ba55f0a0e8b531bb8d4928bf3b3bc6a28d18783
Yaroslav4444-bauer <yarik.bauer.222@gmail.com>
2025-04-23 17:47:09 +0300

    Main Map Russia

--- src/pages/Level.js
+++ src/pages/Level.js
@@ -1,33 +1,109 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from "./img/snapedit_1745216213383.jpeg";
+import './Levels.css';
+
 
 function Level() {
-  const [answer, setAnswer] = useState('');
-  const [result, setResult] = useState('');
-
-  const checkAnswer = () => {
-    if (answer.toLowerCase() === 'дядя ваня') {
-      setResult('Правильно! Переходите на следующий уровень.');
-    } else {
-      setResult('Неверно. Попробуйте еще раз!');
-    }
-  };
-
   return (
-    <div className="level">
-      <h2>Уровень 1: Загадка про дядю Ваню</h2>
-      <p>Кто ездит на самокате и всегда опаздывает на работу?</p>
-      <input
-        type="text"
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-        placeholder="Введите ответ"
-      />
-      <button onClick={checkAnswer}>Проверить</button>
-      <p>{result}</p>
-      <Link to="/">
-        <button>Вернуться на главную</button>
-      </Link>
+    <div>
+      <h1 className='title-map'>Выбери регион для начала игры</h1>
+      <div className="level">
+        <button className='choise-region c0'></button>
+        <button className='choise-region c1'></button>
+        <button className='choise-region c2'></button>
+        <button className='choise-region c3'></button>
+        <button className='choise-region c4'></button>
+        <button className='choise-region c5'></button>
+        <button className='choise-region c6'></button>
+        <button className='choise-region c7'></button>
+        <button className='choise-region c8'></button>
+        <button className='choise-region c9'></button>
+        <button className='choise-region c10'></button>
+        <button className='choise-region c11'></button>
+        <button className='choise-region c12'></button>
+        <button className='choise-region c13'></button>
+        <button className='choise-region c14'></button>
+        <button className='choise-region c15'></button>
+        <button className='choise-region c16'></button>
+        <button className='choise-region c17'></button>
+        <button className='choise-region c18'></button>
+        <button className='choise-region c19'></button>
+        <button className='choise-region c20'></button>
+        <button className='choise-region c21'></button>
+        <button className='choise-region c22'></button>
+        <button className='choise-region c23'></button>
+        <button className='choise-region c24'></button>
+        <button className='choise-region c25'></button>
+        <button className='choise-region c26'></button>
+        <button className='choise-region c27'></button>
+        <button className='choise-region c28'></button>
+        <button className='choise-region c29'></button>
+        <button className='choise-region c30'></button>
+        <button className='choise-region c31'></button>
+        <button className='choise-region c32'></button>
+        <button className='choise-region c33'></button>
+        <button className='choise-region c34'></button>
+        <button className='choise-region c35'></button>
+        <button className='choise-region c36'></button>
+        <button className='choise-region c37'></button>
+        <button className='choise-region c38'></button>
+        <button className='choise-region c39'></button>
+        <button className='choise-region c40'></button>
+        <button className='choise-region c41'></button>
+        <button className='choise-region c42'></button>
+        <button className='choise-region c43'></button>
+        <button className='choise-region c44'></button>
+        <button className='choise-region c45'></button>
+        <button className='choise-region c46'></button>
+        <button className='choise-region c47'></button>
+        <button className='choise-region c48'></button>
+        <button className='choise-region c49'></button>
+        <button className='choise-region c50'></button>
+        <button className='choise-region c51'></button>
+        <button className='choise-region c52'></button>
+        <button className='choise-region c53'></button>
+        <button className='choise-region c54'></button>
+        <button className='choise-region c55'></button>
+        <button className='choise-region c56'></button>
+        <button className='choise-region c57'></button>
+        <button className='choise-region c58'></button>
+        <button className='choise-region c59'></button>
+        <button className='choise-region c60'></button>
+        <button className='choise-region c61'></button>
+        <button className='choise-region c62'></button>
+        <button className='choise-region c63'></button>
+        <button className='choise-region c64'></button>
+        <button className='choise-region c65'></button>
+        <button className='choise-region c66'></button>
+        <button className='choise-region c67'></button>
+        <button className='choise-region c68'></button>
+        <button className='choise-region c69'></button>
+        <button className='choise-region c70'></button>
+        <button className='choise-region c71'></button>
+        <button className='choise-region c72'></button>
+        <button className='choise-region c73'></button>
+        <button className='choise-region c74'></button>
+        <button className='choise-region c75'></button>
+        <button className='choise-region c76'></button>
+        <button className='choise-region c77'></button>
+        <button className='choise-region c78'></button>
+        <button className='choise-region c79'></button>
+        <button className='choise-region c80'></button>
+        <button className='choise-region c81'></button>
+        <button className='choise-region c82'></button>
+        <button className='choise-region c83'></button>
+        <button className='choise-region c84'></button>
+        <button className='choise-region c85'></button>
+        <button className='choise-region c86'></button>
+        <button className='choise-region c87'></button>
+        <button className='choise-region c88'></button>
+        <button className='choise-region c89'></button>
+        <button className='choise-region c90'></button>
+        <button className='choise-region c91'></button>
+        <button className='choise-region c92'></button>
+      </div>
+      
     </div>
   );
 }
